7ec4c2531b3cab20dd3b0e29adb3e7a859c36626
JMerca14 <jcmercado@student.fullsail.edu>
2022-11-06 19:23:09 -0500

    Initial Commit

---
+++ CalorieApp_V1_SwiftUI/CalorieApp_V1_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieApp_V1_SwiftUIApp.swift
+//  CalorieApp_V1_SwiftUI
+//
+//  Created by Juan Mercado on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CalorieApp_V1_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
