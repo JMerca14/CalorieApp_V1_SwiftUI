ff83f8cea288cffe57256a687a268181165cfc36
Blake Lawson <wblawson@student.fullsail.edu>
2022-11-10 13:57:16 -0600

    Fixed light/dark mode for status bar of app. (app does nothing when dark/light mode is changed and "appears" in dark mode.

--- CalorieApp_V1_SwiftUI/CalorieApp_V1_SwiftUIApp.swift
+++ CalorieApp_V1_SwiftUI/CalorieApp_V1_SwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CalorieApp_V1_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().preferredColorScheme(.dark)
         }
     }
 }
